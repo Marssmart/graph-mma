51cae2b399125109833ec02d6f5265c3f27e08cd
jan.srnicek <marssmart1992@gmail.com>
2018-08-15 20:13:01 +0200

    One file approach

--- mma-stats-trigger-apps/src/main/java/org/deer/mma/stats/trigger/apps/FighterLinkScrapingScenarioRunner.java
+++ mma-stats-trigger-apps/src/main/java/org/deer/mma/stats/trigger/apps/FighterLinkScrapingScenarioRunner.java
@@ -22,10 +22,27 @@ public class FighterLinkScrapingScenarioRunner {
 
   public static void main(String[] args) throws IOException, InterruptedException {
     LOG.info("Starting scraping client");
+    if(args.length != 1){
+      LOG.info("Illegal arguments detected : {}",Arrays.toString(args));
+      System.exit(-1);
+    }
     final OkHttpClient client = new OkHttpClient();
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      timer.cancel();
+      timer.purge();
+      client.dispatcher().executorService().shutdownNow();
+      client.connectionPool().evictAll();
+      if (client.cache() != null) {
+        try {
+          client.cache().close();
+        } catch (IOException e) {
+          throw new IllegalStateException(e);
+        }
+      }
+    }));
 
-    LOG.info("Sending scraping request for links {}", Arrays.toString(args));
-    triggerScraping(client,args);
+    LOG.info("Sending scraping request for links {}", args[0]);
+    triggerScraping(client,args[0]);
 
     Request checkRequest = createCheckScrapingRequest();
     boolean isRunning = true;
@@ -43,17 +60,11 @@ public class FighterLinkScrapingScenarioRunner {
         }
       }
     }
-    timer.cancel();
-    timer.purge();
-    client.dispatcher().executorService().shutdownNow();
-    client.connectionPool().evictAll();
-    if (client.cache() != null) {
-      client.cache().close();
-    }
+
     LOG.info("Scraping finished");
   }
 
-  private static void triggerScraping(OkHttpClient client, String[] links) throws IOException {
+  private static void triggerScraping(OkHttpClient client, String links) throws IOException {
     final Response triggerResponse = client.newCall(createScrapingRequest(links)).execute();
 
     if (!triggerResponse.isSuccessful()) {
@@ -72,9 +83,9 @@ public class FighterLinkScrapingScenarioRunner {
     }, TimeUnit.MINUTES.toMillis(60));
   }
 
-  private static Request createScrapingRequest(String[] links) {
+  private static Request createScrapingRequest(String links) {
     MediaType mediaType = MediaType.parse("application/json");
-    RequestBody body = RequestBody.create(mediaType, Arrays.toString(links));
+    RequestBody body = RequestBody.create(mediaType, links);
     return new Request.Builder()
         .url("http://localhost:8445/api/reactor/trigger-scraping")
         .post(body)
