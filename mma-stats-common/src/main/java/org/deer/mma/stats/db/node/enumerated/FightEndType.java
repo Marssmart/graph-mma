378f8fdd378f1f1eb1b8c498e2f67c7bf6fb3c06
jan.srnicek <marssmart1992@gmail.com>
2018-08-20 14:31:49 +0200

    Completition of FightEndType enum

--- mma-stats-common/src/main/java/org/deer/mma/stats/db/node/enumerated/FightEndType.java
+++ mma-stats-common/src/main/java/org/deer/mma/stats/db/node/enumerated/FightEndType.java
@@ -2,5 +2,37 @@ package org.deer.mma.stats.db.node.enumerated;
 
 public enum FightEndType {
 
-  DECISION_SPLIT;
+  DECISION_SPLIT,
+  DECISION_UNANIMOUS,
+  DECISION_MAJORITY,
+  TKO,
+  KO,
+  SUBMISSION,
+  N_A;
+
+  public static FightEndType match(final String input) {
+    final String standardInput = input.trim().toLowerCase();
+
+    if (standardInput.contains("decision")) {
+      if (standardInput.contains("unanimous")) {
+        return DECISION_UNANIMOUS;
+      }
+      if (standardInput.contains("major")) {
+        return DECISION_MAJORITY;
+      }
+      return DECISION_SPLIT;
+    }
+
+    if (standardInput.contains("ko")) {
+      if (standardInput.contains("tko")) {
+        return TKO;
+      }
+      return KO;
+    }
+
+    if (standardInput.contains("sub")) {
+      return SUBMISSION;
+    }
+    return N_A;
+  }
 }
